43d7aeb4b28b7eb10505c5c03badcb4b9ef6edde
Caio André <caio_andre15@hotmail.com>
2020-07-31 23:24:07 -0300

    Creation of the BusinessAccount class that extends the Account class in the entities package

---
+++ src/entities/BusinessAccount.java
@@ -0,0 +1,34 @@
+package entities;
+
+public class BusinessAccount extends Account {
+
+	// Attributes
+	private Double loanLimit;
+
+	// Builders
+	public BusinessAccount() {
+		super();
+	}
+
+	public BusinessAccount(Integer number, String holder, Double balance, Double loanLimit) {
+		super(number, holder, balance);
+		this.loanLimit = loanLimit;
+	}
+
+	// Accessor Methods
+	public Double getLoanLimit() {
+		return loanLimit;
+	}
+	
+	public void setLoanLimit(Double loanLimit) {
+		this.loanLimit = loanLimit;
+	}
+
+	// Methods
+	public void loan(Double amount) {
+		if (amount <= loanLimit) {
+			deposit(amount);
+		}
+	}
+
+}
