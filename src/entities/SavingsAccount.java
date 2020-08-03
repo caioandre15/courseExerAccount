db00183fde9d3c932fd7ad9f352c085a6ddf305d
Caio André <caio_andre15@hotmail.com>
2020-08-03 15:34:26 -0300

    SavingsAccount class created in entities package

---
+++ src/entities/SavingsAccount.java
@@ -0,0 +1,32 @@
+package entities;
+
+public class SavingsAccount extends Account {
+
+	// Attributes
+	private Double interestRate;
+
+	// Builders
+	public SavingsAccount() {
+		super();
+	}
+
+	public SavingsAccount(Integer number, String holder, Double balance, Double interestRate) {
+		super(number, holder, balance);
+		this.interestRate = interestRate;
+	}
+
+	// Accessor Methods
+	public Double getInterestRate() {
+		return interestRate;
+	}
+
+	public void setInterestRate(Double interestRate) {
+		this.interestRate = interestRate;
+	}
+
+	// Methods
+	public void updateBalance() {
+		balance += balance * interestRate;
+	}
+
+}
